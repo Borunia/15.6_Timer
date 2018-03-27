326f3150fb926f01a767ad4cf7f95b786ee7c022
Borunia <borekosp@gmail.com>
2018-03-27 23:13:19 +0200

    add stoper

---
+++ script.js
@@ -0,0 +1,97 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    resetClock() {
+        this.reset();
+        this.print();
+    }
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+const stopwatch = new Stopwatch(
+document.querySelector('.stopwatch'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let clearButton = document.getElementById('reset');
+clearButton.addEventListener('click', () => stopwatch.resetClock());
+
+let saveScore = document.getElementById('save');
+saveScore.addEventListener('click', function () {
+    var list = document.createElement('li');
+    var results = document.querySelector('.stopwatch').textContent;
+        list.innerHTML = results;
+    document.getElementById('results').appendChild(list);
+  }
+);
+
+let resetSaveScore = document.getElementById('resetBoard');
+resetSaveScore.addEventListener('click', function () {
+    var appendlist = document.getElementsByTagName('li');
+    var results = document.getElementById('results');
+        console.log(appendlist);
+    results.removeChild(appendlist[0]);
+    }
+);
